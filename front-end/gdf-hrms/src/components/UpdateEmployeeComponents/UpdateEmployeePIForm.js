2a5ae81715a460af4218137577858e7b82fe3f2d
Stephanie Simon <37277157+JacintaSimon@users.noreply.github.com>
2021-05-10 13:40:43 -0400

    Revert "Revert "Field was removed from First and Last Name for the UpdateEmployeePIForm""
    This reverts commit dff6878a95a5c38e036d55a8269cfecd55f32a2b.

--- front-end/gdf-hrms/src/components/UpdateEmployeeComponents/UpdateEmployeePIForm.js
+++ front-end/gdf-hrms/src/components/UpdateEmployeeComponents/UpdateEmployeePIForm.js
@@ -129,7 +129,7 @@ export default function UpdateEmployeePIForm(props) {
             <React.Fragment>
               
                 <Grid item xs={2}>
-                  <Field as ={TextField} 
+                  <TextField
                          name="firstName" 
                          label="First Name" 
                          InputLabelProps={{ shrink: true,}} 
@@ -146,7 +146,7 @@ export default function UpdateEmployeePIForm(props) {
                 </Grid>
 
                 <Grid item xs={2}>
-                <Field as= {TextField} 
+                <TextField 
                        name="lastName" 
                        name="lastName" 
                        label="Last Name" 
